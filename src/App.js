28c3a12076e15eaa2babf01085e07c9a9b298e58
Franco Aguilera <flaalf@gmail.com>
2021-06-14 21:44:49 -0300

    add UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import TodoForm from './components/todo-form/'
+import TodoItemList from './components/todo-item/'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App col-md-8 offset-md-2">
+      <TodoForm />
+      <TodoItemList />
     </div>
   );
 }
